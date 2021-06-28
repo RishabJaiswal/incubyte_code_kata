1e4025c80c299c9d24b026b29edd0d971d3a0565
Rishab Jaiswal <>
2021-06-28 13:30:12 +0530

    Refactor
    - separate method to find sum of all inputs
    - merge methods for 2 or more inputs

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,11 +1,8 @@
 public class StringCalculator {
 
-    private int addTwoInputs(String input1, String input2) {
-        int int1 = Integer.parseInt(input1);
-        int int2 = Integer.parseInt(input2);
-        return int1 + int2;
-    }
-
+    /**
+     * single input
+     */
     private int parseSingleInput(String input) {
         if (input.equals("")) {
             return 0;
@@ -15,19 +12,24 @@ public class StringCalculator {
     }
 
     /**
+     * multiple inputs
+     */
+    private int addInputs(String... inputs) {
+        int sum = 0;
+        for (String input : inputs) {
+            sum += Integer.parseInt(input);
+        }
+        return sum;
+    }
+
+    /**
      * add given inputs
      */
     public int add(String... inputs) {
         if (inputs.length == 1) {
             return parseSingleInput(inputs[0]);
-        } else if (inputs.length == 2) {
-            return addTwoInputs(inputs[0], inputs[1]);
         } else {
-            int sum = 0;
-            for (String input : inputs) {
-                sum += Integer.parseInt(input);
-            }
-            return sum;
+            return addInputs(inputs);
         }
     }
 }
