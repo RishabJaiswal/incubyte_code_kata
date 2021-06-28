8d725bd93927280f5f3d8a807fab1adb3bab9593
Rishab Jaiswal <>
2021-06-28 13:18:39 +0530

    Refactor
    - divide large method into smalled methods

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,19 +1,27 @@
 public class StringCalculator {
 
+    private int addTwoInputs(String input1, String input2) {
+        int int1 = Integer.parseInt(input1);
+        int int2 = Integer.parseInt(input2);
+        return int1 + int2;
+    }
+
+    private int parseSingleInput(String input) {
+        if (input.equals("")) {
+            return 0;
+        } else {
+            return Integer.parseInt(input);
+        }
+    }
+
     /**
      * add given inputs
      */
     public int add(String... inputs) {
         if (inputs.length == 1) {
-            if (inputs[0].equals("")) {
-                return 0;
-            } else {
-                return Integer.parseInt(inputs[0]);
-            }
+            return parseSingleInput(inputs[0]);
         } else {
-            int input1 = Integer.parseInt(inputs[0]);
-            int input2 = Integer.parseInt(inputs[1]);
-            return input1 + input2;
+            return addTwoInputs(inputs[0], inputs[1]);
         }
     }
 }
